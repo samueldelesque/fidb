d72fd9482c9e58410441c384e2181e27691b9743
Sam Delesque <sdelesque@domanistudios.com>
2015-05-15 15:59:33 -0400

    rm csv tmp files

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var Request = require("request"),
-	fs = require("fs"),
 	Csv = require("fast-csv"),
 	Mongolian = require('mongolian'),
 	forEach = require("for-each"),
@@ -14,16 +13,7 @@ var Request = require("request"),
 	provider = "http://download.finance.yahoo.com/d/quotes.csv?",
 
 	sp500quotes = "A,AA,AAPL,ABC,ABT,ACE,ACN,ADBE,ADI,ADM,ADP,ADSK,ADT,AEE,AEP,AES,AET,AFL,AGN,AIG,AIV,AIZ,AKAM,ALL,ALTR,ALXN,AMAT,AMD,AMGN,AMP,AMT,AMZN,AN,ANF,AON,APA,APC,APD,APH,APOL,ARG,ATI,AVB,AVP,AVY,AXP,AZO,BA,BAC,BAX,BBBY,BBT,BBY,BCR,BDX,BEAM,BEN,BF.B,BHI,BIG,BIIB,BK,BLK,BLL,BMC,BMS,BMY,BRCM,BRK.B,BSX,BTU,BWA,BXP,C,CA,CAG,CAH,CAM,CAT,CB,CBG,CBS,CCE,CCI,CCL,CELG,CERN,CF,CFN,CHK,CHRW,CI,CINF,CL,CLF,CLX,CMA,CMCSA,CME,CMG,CMI,CMS,CNP,CNX,COF,COG,COH,COL,COP,COST,COV,CPB,CRM,CSC,CSCO,CSX,CTAS,CTL,CTSH,CTXS,CVC,CVH,CVS,CVX,D,DD,DE,DELL,DF,DFS,DG,DGX,DHI,DHR,DIS,DISCA,DLTR,DNB,DNR,DO,DOV,DOW,DPS,DRI,DTE,DTV,DUK,DVA,DVN,EA,EBAY,ECL,ED,EFX,EIX,EL,EMC,EMN,EMR,EOG,EQR,EQT,ESRX,ESV,ETFC,ETN,ETR,EW,EXC,EXPD,EXPE,F,FAST,FCX,FDO,FDX,FE,FFIV,FHN,FII,FIS,FISV,FITB,FLIR,FLR,FLS,FMC,FOSL,FRX,FSLR,FTI,FTR,GAS,GCI,GD,GE,GILD,GIS,GLW,GME,GNW,GOOG,GPC,GPS,GS,GT,GWW,HAL,HAR,HAS,HBAN,HCBK,HCN,HCP,HD,HES,HIG,HNZ,HOG,HON,HOT,HP,HPQ,HRB,HRL,HRS,HSP,HST,HSY,HUM,IBM,ICE,IFF,IGT,INTC,INTU,IP,IPG,IR,IRM,ISRG,ITW,IVZ,JBL,JCI,JCP,JDSU,JEC,JNJ,JNPR,JOY,JPM,JWN,K,KEY,KIM,KLAC,KMB,KMI,KMX,KO,KR,KRFT,KSS,L,LEG,LEN,LH,LIFE,LLL,LLTC,LLY,LM,LMT,LNC,LO,LOW,LRCX,LSI,LTD,LUK,LUV,LYB,M,MA,MAR,MAS,MAT,MCD,MCHP,MCK,MCO,MDLZ,MDT,MET,MHP,MJN,MKC,MMC,MMM,MNST,MO,MOLX,MON,MOS,MPC,MRK,MRO,MS,MSFT,MSI,MTB,MU,MUR,MWV,MYL,NBL,NBR,NDAQ,NE,NEE,NEM,NFLX,NFX,NI,NKE,NOC,NOV,NRG,NSC,NTAP,NTRS,NU,NUE,NVDA,NWL,NWSA,NYX,OI,OKE,OMC,ORCL,ORLY,OXY,PAYX,PBCT,PBI,PCAR,PCG,PCL,PCLN,PCP,PCS,PDCO,PEG,PEP,PETM,PFE,PFG,PG,PGR,PH,PHM,PKI,PLD,PLL,PM,PNC,PNR,PNW,POM,PPG,PPL,PRGO,PRU,PSA,PSX,PWR,PX,PXD,QCOM,QEP,R,RAI,RDC,RF,RHI,RHT,RL,ROK,ROP,ROST,RRC,RRD,RSG,RTN,S,SAI,SBUX,SCG,SCHW,SE,SEE,SHW,SIAL,SJM,SLB,SLM,SNA,SNDK,SNI,SO,SPG,SPLS,SRCL,SRE,STI,STJ,STT,STX,STZ,SWK,SWN,SWY,SYK,SYMC,SYY,T,TAP,TDC,TE,TEG,TEL,TER,TGT,THC,TIE,TIF,TJX,TMK,TMO,TRIP,TROW,TRV,TSN,TSO,TSS,TWC,TWX,TXN,TXT,TYC,UNH,UNM,UNP,UPS,URBN,USB,UTX,V,VAR,VFC,VIAB,VLO,VMC,VNO,VRSN,VTR,VZ,WAG,WAT,WDC,WEC,WFC,WFM,WHR,WIN,WLP,WM,WMB,WMT,WPI,WPO,WPX,WU,WY,WYN,WYNN,X,XEL,XL,XLNX,XOM,XRAY,XRX,XYL,YHOO,YUM,ZION,ZMH",
-	queue = [],
 	interval = 5 * 60 * 1000
-
-
-var savePath = function(time,format){
-	var m = time.getMonth()
-	var d = time.getDate()
-	var addLeadingZero = function(n){return (n < 10)?"0"+n:n}
-	return time.getFullYear()+"-"+addLeadingZero(m)+"-"+addLeadingZero(d)+"-"+addLeadingZero(time.getHours())+"-"+addLeadingZero(time.getMinutes())+"."+format
-}
 
 function save_data(symbols,format){
 	if(!symbols||!format)return false
@@ -32,23 +22,22 @@ function save_data(symbols,format){
 			var now = new Date(),
 				path = "csv/"+savePath(now,"csv")
 
-			fs.writeFile(path, body, function(){
-				queue.push(path)
-			});
-
 			Csv.fromString(body, {headers: [
 				"symbol",
 				"name",
 				"price"
 			]}).on("data", function(data){
-				data.price = parseFloat(data.price)
-				data.time = now
-				sp500.insert(data)
+                        if(!data.price || !data.name, || data.symbol){
+                              //freak out
+                        }
+                        else{
+      				data.price = parseFloat(data.price)
+      				data.time = now
+      				sp500.insert(data)
+                        }
 			}).on("end", function(){
-					console.log(path,"was processed");
+				// tell api test service to not freak out just then.
 			});
-
-			// console.log(queue);
 		}
 	});
 }
